90b8305dd26842c006a02d173d526ff3c65a8448
Антон Селифанов <tatoxer@gmail.com>
2023-08-15 17:36:48 +0300

    add form logic

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const form = document.querySelector('.form');
-const button = document.querySelector('button');
+const button = document.querySelector('.form-button');
+const label = document.querySelector('.form-label');
+const input = document.querySelector('.form-input');
+const resultText = document.querySelector('.result-text');
 
-const getFormData = (form) => {
+const calculateNDC = (form) => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const number = Number(formData.get('number'));
     if (isNaN(number)) {
-      // button.textContent('Нужно ввести число');
-      console.log(button);
+      label.textContent = 'Нужно ввести число';
+      label.classList.add('red');
     }
-    return number;
+
+    button.classList.add('hidden');
+    label.classList.add('hidden');
+    input.classList.add('hidden');
+    resultText.classList.remove('hidden');
+
+    resultText.textContent = number;
   });
 };
 
-getFormData(form);
+calculateNDC(form);
